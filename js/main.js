46bd39b8eba757361a43a69436731e610baceb1a
config <liza-camacho@hotmail.com>
2025-05-05 22:18:20 -0600

    Subiendo codigo js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*Crear una función que se va a llamar ejemplo
+y tiene que devolver un string diciendo; ”Hola, soy
+una función”*/
+function ejemplo(){
+    return 'Hola, soy una función';
+ }
+console.log(ejemplo())
